c139605e6433b770851b639dcabb7598a38a5179
arthick998 <rakshitbhagat9582@gmail.com>
2022-10-31 21:11:27 +0530

    files linking

--- fundamental 2/lessons.js
+++ fundamental 2/lessons.js
@@ -219,3 +219,12 @@ for (let exercise = 1; exercise < 4; exercise++) {
   }
 }
 */
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} `);
+}
+
+let rep = 1;
+while (rep <= 10) {
+  console.log(`WHILE: Lifting weighs repetition ${rep}`);
+  rep++;
+}
